bd79c6fc4afcc8dde3115049186f74f36008c03c
phantuananh1206 <phantuananhltt@gmail.com>
2021-03-14 15:08:39 +0700

    Gem devise

--- db/schema.rb
+++ db/schema.rb
@@ -144,7 +144,6 @@ ActiveRecord::Schema.define(version: 2021_03_14_043408) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
-    t.string "email"
     t.string "address"
     t.string "phone_number"
     t.string "password"
