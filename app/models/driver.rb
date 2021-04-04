2b8e0419a8f1c0ab9663b7a0be178925fb8804fb
DiemQuynh21 <lediemquynh999@gmail.com>
2021-04-04 11:59:26 +0700

    Sign-in Partner_page

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -50,4 +50,11 @@ class Driver < ApplicationRecord
       transitions from: [:offline, :online], to: :locked
     end
   end
+  before_save :downcase_email
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
 end
