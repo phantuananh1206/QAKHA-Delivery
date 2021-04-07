47369235bc8cc09f45e3829069d1afe50524a854
phantuananh1206 <phantuananhltt@gmail.com>
2021-04-07 21:59:10 +0700

    CRUD Cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,9 @@ class Cart < ApplicationRecord
   belongs_to :product
   belongs_to :user
   belongs_to :partner
+
+  validates :quantity, presence: true,
+            numericality: {only_integer: true,
+                           greater_than:
+                           Settings.validation.number.zero}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,10 +21,13 @@ Rails.application.routes.draw do
           get "test_method", to: 'users#test_method'
           post "check_email", to: 'registrations#check_email_exits'
           post "check_phone_number", to: 'registrations#check_phone_number_exits'
+          delete "/clear_cart", to: 'carts#clear_cart'
+          get "/cart", to: 'carts#show'
         end
         resources :users, only: %i(index show update)
         resources :types, only: %i(index show)
         resources :partners, only: %i(index)
+        resource :carts, only: %i(create update destroy)
       end
     end
 
