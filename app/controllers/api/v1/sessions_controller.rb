ddc62f579dac7364912975f114ce3bef47a50d6c
DiemQuynh21 <lediemquynh999@gmail.com>
2021-04-17 23:51:35 +0700

    API Sign up for partner and driver

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::SessionsController < Devise::SessionsController
   before_action :load_user_authentication, only: [:create, :destroy]
+  before_action :load_driver_authentication, only: [:sign_in_driver, :destroy]
+
   skip_before_action :verify_authenticity_token
   before_action :load_address, only: :create
 
@@ -19,12 +21,31 @@ class Api::V1::SessionsController < Devise::SessionsController
       invalid_login_attempt
   end
 
+  def sign_in_driver
+    if @driver.valid_password? params[:password]
+      sign_in @driver, store: false
+      jwt = JWT.encode(
+        { name: @driver.name, id: @driver.id, exp: (Time.now + 2.hours).to_i },
+        Rails.application.secrets.secret_key_base,
+        'HS256'
+      )
+      render json: { message: "Log in successful", token: jwt }, status: :ok
+      return
+    end
+      invalid_login_attempt
+  end
+
   private
 
   def load_user_authentication
     @user = User.find_by_email params[:email]
 
     render json: {message: "Email is not exists. Please sign up !!"}, status: :bad_request unless @user
+  end
+
+  def load_driver_authentication
+    @driver = Driver.find_by_email params[:email]
+    render json: {message: "Email is not exists. Please sign up !!"}, status: :bad_request unless @driver
   end
 
   def user_params
