0f9cb3f91e713116a6e8242323958df043d6b4fa
phantuananh1206 <phantuananhltt@gmail.com>
2021-04-23 15:26:20 +0700

    Admin manage vouchers

--- app/models/user.rb
+++ app/models/user.rb
@@ -80,8 +80,8 @@ class User < ApplicationRecord
   def self.to_xls
     CSV.generate do |csv|
       csv << column_names
-      all.each do |driver|
-        csv << driver.attributes.values_at(*column_names)
+      all.each do |user|
+        csv << user.attributes.values_at(*column_names)
       end
     end
   end
