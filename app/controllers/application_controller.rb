997a6fab48d4594c192854b9ec8bac703c5e849f
phantuananh1206 <phantuananhltt@gmail.com>
2021-04-24 11:21:11 +0700

    Update status partner and voucher

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # before_action :load_partners
+  before_action :load_partners, :load_voucher
   include SessionsHelper
 
   private
@@ -25,10 +25,21 @@ class ApplicationController < ActionController::Base
   def load_partners
     @partners = Partner.all
     @partners.each do |partner|
-      if Time.zone.now.strftime('%H:%M') >= partner.time_open.strftime('%H:%M') && partner.close?
-        partner.update(status: :open)
+      if Time.zone.now.strftime('%H:%M') >= partner.time_open.strftime('%H:%M') && Time.zone.now.strftime('%H:%M') <= partner.time_close.strftime('%H:%M') && partner.close?
+        partner.update_columns(status: :open)
       elsif Time.zone.now.strftime('%H:%M') >= partner.time_close.strftime('%H:%M') && partner.open?
-        partner.update(status: :close)
+        partner.update_columns(status: :close)
+      end
+    end
+  end
+
+  def load_voucher
+    @vouchers = Voucher.all
+    @vouchers.each do |voucher|
+      if Time.zone.now >= voucher.expiry_date && voucher.effective?
+        voucher.update_columns(status: :void)
+      elsif Time.zone.now < voucher.expiry_date && voucher.void?
+        voucher.update_columns(status: :effective)
       end
     end
   end
